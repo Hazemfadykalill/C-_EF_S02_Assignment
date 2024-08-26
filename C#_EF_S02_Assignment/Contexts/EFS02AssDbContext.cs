d8fa1d058803d32837cd56b4919b821ce477c6c1
Hazem Fady <hazemfady123@gmail.com>
2024-08-26 20:50:31 +0300

    Add project files.

---
+++ C#_EF_S02_Assignment/Contexts/EFS02AssDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__EF_S02_Assignment.Contexts
+{
+    public class EFS02AssDbContext:DbContext
+    {
+
+        //To Connect Database
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(" Server = .;Database=EFS02Assignment;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+
+        //Configuration Class
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+
+        }
+        //Names Table That In Database
+
+    }
+}
---
+++ C#_EF_S02_Assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C__EF_S02_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
